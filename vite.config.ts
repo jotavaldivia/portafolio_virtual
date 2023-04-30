34c6e1513bfcc6c820c957c189ac5db57f59fad9
Juan Valdebenito <valdebenitovaldiviajuan@gmail.com>
2023-04-30 10:52:42 -0400

    base upadte in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portafolio_virtual/',
+  base: 'https://github.com/jotavaldivia/portafolio_virtual/',
 });
