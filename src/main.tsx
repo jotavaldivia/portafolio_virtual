f05120df9166afcb4bce75453eeefe4df7d43790
Juan Valdebenito <valdebenitovaldiviajuan@gmail.com>
2023-04-30 11:52:57 -0400

    add baseurl in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,30 +3,33 @@ import { Layout } from './pages/layouts';
 import { Home, Error404, About, Experiencia } from './pages';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/sobremi',
-        element: <About />,
-      },
-      {
-        path: '/experiencia',
-        element: <Experiencia />,
-      },
-    ],
-  },
-  {
-    path: '*',
-    element: <Error404 />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/sobremi',
+          element: <About />,
+        },
+        {
+          path: '/experiencia',
+          element: <Experiencia />,
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <Error404 />,
+    },
+  ],
+  { basename: '/portafolio_virtual' }
+);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RouterProvider router={router} />
 );
