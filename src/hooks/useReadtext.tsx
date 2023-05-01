baa11f142902b851a5df371fcc7f61e42e2b9829
Juan Valdebenito <valdebenitovaldiviajuan@gmail.com>
2023-04-30 23:39:08 -0400

    update: useReadtext.txs

--- src/hooks/useReadtext.tsx
+++ src/hooks/useReadtext.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-
 const useReadtext = (text: string) => {
   const [isMuted, setIsMuted] = useState<boolean>(true);
   const toggleMute = (): void => {
@@ -32,8 +31,27 @@ const useReadtext = (text: string) => {
     partesDeTexto.forEach(function (parte) {
       // Crear un objeto SpeechSynthesisUtterance para representar cada parte del texto
       const mensaje = new SpeechSynthesisUtterance();
-      mensaje.rate = 1.21;
+      mensaje.rate = 1.2;
+      mensaje.pitch = 1;
+      mensaje.lang = 'es-ES';
       mensaje.text = parte; // Establecer el texto que se va a leer en el mensaje
+      // Obtener la lista de voces disponibles en el navegador
+      const voices = window.speechSynthesis.getVoices();
+
+      // Encontrar una voz masculina en español neutro
+      const voice = voices?.find(function (voice) {
+        return (
+          voice.lang === 'es-ES' &&
+          voice.name === 'Microsoft David Desktop - Spanish (Spain)'
+        );
+      });
+
+      // Configurar la voz del mensaje
+      if (voice) {
+        mensaje.voice = voice;
+      } else {
+        console.error('No se encontró una voz masculina en español neutro');
+      }
       window.speechSynthesis.speak(mensaje); // Leer el mensaje en voz alta utilizando el método speak de la API de Speech Synthesis
     });
   };
